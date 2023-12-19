ae7cf3b99f189409715f94880509076e1dc27144
Cristina Rodriguez <yosola@gmail.com>
2023-12-19 15:25:50 -0800

    Day two working

---
+++ day02/day02.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+const inputarr = fs.readFileSync(path.join(__dirname, './example.txt'), 'utf8').split('\n');
+
+
+function partOne() {
+    let runningTotal = 0;
+    const maxcubes = { 'red': 12, 'green': 13, 'blue': 14 }
+
+    inputarr.forEach( (value) => {
+        let [game, sets] = value.split(':').map(i => i.trim());
+        let gameid = Number(game.match(/(\d+$)/)[0]);
+        let set = sets.split(';').map(i => i.trim());
+        let possible = true;
+
+        set.forEach( (value) => {
+            let cubes = value.split(",").map(i => i.trim());
+            cubes.forEach(function(cube) {
+                let [count, color] = cube.split(' ').map(i => i.trim());
+                if (Number(count) > maxcubes[color]) { possible = false; }
+            });
+        });
+        if (possible) { runningTotal += gameid; }
+    });
+    console.log(`Part One: ${runningTotal}`);
+}
+
+function partTwo() {
+    let runningTotal = 0;
+    inputarr.forEach( (value) => {
+        let [ , sets] = value.split(':').map(i => i.trim());
+        let set = sets.split(/[;,]/).map(i => i.trim());
+        let cubeMinimum = { 'red': 0, 'green': 0, 'blue': 0 }
+
+        set.forEach(function(cube) {
+            let [count, color] = cube.split(' ').map(i => i.trim());
+            if (cubeMinimum[color] < Number(count)) { cubeMinimum[color] = Number(count); }
+        });
+        runningTotal += Object.values(cubeMinimum).reduce( (p, n) => p*n, 1); 
+    });
+    console.log(`Part Two: ${runningTotal}`);
+}
+
+partOne();
+partTwo();
